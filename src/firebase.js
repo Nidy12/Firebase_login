424d82c163ca0606bcef343111ebed56690283c4
Nidhi <nidhipoojary1211@gmail.com>
2024-06-14 20:20:39 +0530

    minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,24 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APPE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDRYjGkGQX51LMneT6a9lLm-HWy5i6MZKU",
-  authDomain: "react-authentication-f9d1d.firebaseapp.com",
-  projectId: "react-authentication-f9d1d",
-  storageBucket: "react-authentication-f9d1d.appspot.com",
-  messagingSenderId: "593225618196",
-  appId: "1:593225618196:web:de42799f9b614275d4e801",
-  measurementId: "G-XQ8SZ7RT9S"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 
